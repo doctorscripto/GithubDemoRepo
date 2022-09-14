10d212e9b8417226f4ad4fd22ac3f7a1e8e79978
Sean Kearney <5871738+doctorscripto@users.noreply.github.com>
2022-09-14 15:25:47 -0400

    Added a new file for the demo

---
+++ testme.ps1
@@ -0,0 +1,23 @@
+<#
+.SYNOPSIS
+   My Demo Script   
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   .\testme.ps1                           
+.EXAMPLE
+   Another example of how to use this cmdlet
+.INPUTS
+   Inputs to this cmdlet (if any)
+.OUTPUTS
+   Output from this cmdlet (if any)
+.NOTES
+   General notes
+.COMPONENT
+   The component this cmdlet belongs to
+.ROLE
+   The role this cmdlet belongs to
+.FUNCTIONALITY
+   The functionality that best describes this cmdlet
+#>
+Write-Output 'Hello World'
